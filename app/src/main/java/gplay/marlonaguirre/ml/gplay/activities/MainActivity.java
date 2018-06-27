291a10381223263cd2ca7ea70b18a39b0aef537e
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2018-06-27 00:30:09 -0600

    splash screen added and new theme

--- app/src/main/java/gplay/marlonaguirre/ml/gplay/activities/MainActivity.java
+++ app/src/main/java/gplay/marlonaguirre/ml/gplay/activities/MainActivity.java
@@ -1,13 +1,15 @@
-package gplay.marlonaguirre.ml.gplay;
+package gplay.marlonaguirre.ml.gplay.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import gplay.marlonaguirre.ml.gplay.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/gplay/marlonaguirre/ml/gplay/activities/SplashActivity.java
@@ -0,0 +1,26 @@
+package gplay.marlonaguirre.ml.gplay.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gplay.marlonaguirre.ml.gplay.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.MyTheme);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
