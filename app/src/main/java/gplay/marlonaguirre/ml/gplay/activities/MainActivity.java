30ce04ff855a2397c790ff8d1bec9e358ae9d8da
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2018-06-28 22:03:34 -0600

    onclicklistener added to folders list

--- app/src/main/java/gplay/marlonaguirre/ml/gplay/activities/MainActivity.java
+++ app/src/main/java/gplay/marlonaguirre/ml/gplay/activities/MainActivity.java
@@ -5,49 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.Toast;
-
 import java.io.File;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-
+import java.util.HashSet;
+import java.util.Set;
 import gplay.marlonaguirre.ml.gplay.R;
+import gplay.marlonaguirre.ml.gplay.adapters.FoldersAdapter;
 import gplay.marlonaguirre.ml.gplay.adapters.SongsAdapter;
+
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerSongs,recyclerFolders;
-    ArrayList<String> songs_list,foilders_list;
+    ArrayList<String> songs_list,folders_list;
+    Set<String> all_folders_list;
     TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         tabHost = (TabHost) findViewById(R.id.tabHost);
-
-
         tabHost.setup();
         TabHost.TabSpec thSongs = tabHost.newTabSpec("Tab1");
         thSongs.setIndicator("Songs");
         thSongs.setContent(R.id.layoutSongs);
 
-
         TabHost.TabSpec thFolders = tabHost.newTabSpec("Tab2");
         thFolders.setIndicator("Folders");
         thFolders.setContent(R.id.layoutFolders);
-
         tabHost.addTab(thSongs);
         tabHost.addTab(thFolders);
 
         recyclerSongs = findViewById(R.id.recyclerSongs);
-
-        foilders_list = new ArrayList<>();
+        recyclerFolders = findViewById(R.id.recyclerFolders);
+        folders_list = new ArrayList<>();
         songs_list = new ArrayList<>();
 
         String path = Environment.getExternalStorageDirectory().getPath();
@@ -65,11 +62,27 @@ public class MainActivity extends AppCompatActivity {
                         songs_list.get(recyclerSongs
                                 .getChildAdapterPosition(view))
                         , Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        HashSet<String> hashSet = new HashSet<String>(folders_list);
+        folders_list.clear();
+        folders_list.addAll(hashSet);
+        FoldersAdapter adapterFolders = new FoldersAdapter(folders_list);
+
+        adapterFolders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        folders_list.get(recyclerFolders
+                                .getChildAdapterPosition(view))
+                        , Toast.LENGTH_SHORT).show();
             }
         });
         recyclerSongs.setLayoutManager( new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerFolders.setLayoutManager( new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
         recyclerSongs.setAdapter(adapter);
+        recyclerFolders.setAdapter(adapterFolders);
     }
 
     public void toggleDetails(View view) {
@@ -78,16 +91,13 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> searchSongs(File path) {
         for(File file : path.listFiles()){
             if(file.isFile()  && file.getName().endsWith(".mp3") && !file.isHidden()){
-                foilders_list.add(file.getAbsoluteFile().getName());
-//                Log.e("archivo: ",file.getName()+" FOLDER: " +file.getParentFile().getName());
+                folders_list.add(file.getParentFile().getName());
                 songs_list.add(file.getName());
             }else{
                 if(file.isDirectory() && !file.isHidden()){
-//                    Log.e("DIRECTORIO",file.getName());
                     searchSongs(file);
                 }
             }
-
         }
         return  songs_list;
     }
---
+++ app/src/main/java/gplay/marlonaguirre/ml/gplay/adapters/FoldersAdapter.java
@@ -0,0 +1,62 @@
+package gplay.marlonaguirre.ml.gplay.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import gplay.marlonaguirre.ml.gplay.R;
+
+public class FoldersAdapter extends RecyclerView.Adapter<FoldersAdapter.ViewHolderFolders>
+implements View.OnClickListener{
+    private View.OnClickListener listener;
+    ArrayList<String> folders_list;
+
+    public FoldersAdapter(ArrayList<String> folders_list) {
+        this.folders_list = folders_list;
+    }
+
+    @NonNull
+    @Override
+    public FoldersAdapter.ViewHolderFolders onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.folder_item_list,null,false);
+        view.setOnClickListener(this);
+        return new ViewHolderFolders(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoldersAdapter.ViewHolderFolders holder, int position) {
+        holder.tvFolderName.setText(folders_list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return folders_list.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (listener!=null){
+            listener.onClick(v);
+        }
+    }
+
+    public class ViewHolderFolders extends RecyclerView.ViewHolder {
+
+        TextView tvFolderName;
+
+        public ViewHolderFolders(View itemView) {
+            super(itemView);
+            tvFolderName = itemView.findViewById(R.id.tvFolder);
+        }
+    }
+}
